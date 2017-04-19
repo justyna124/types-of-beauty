8cc941241147e2efb27f64e61a4e0630df3be9e3
justyna124 <justyna124@interia.pl>
2017-04-19 09:30:05 +0200

    gh pages configurations

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt)
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-
+    grunt.loadNpmTasks('grunt-gh-pages');
     require('load-grunt-tasks')(grunt);
 
 
@@ -16,61 +16,66 @@ module.exports = function (grunt)
     };
 
     grunt.initConfig({
-                config: config,
-                watch: {
-                    livereload: {
-                        options: {
-                            livereload: '<%= connect.options.livereload %>'
-                        },
-                        files: ['<%= config.app %>/index.html','<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js','<%= config.app %>/**/*.css']
-                    }
+        config: config,
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
                 },
+                files: ['<%= config.app %>/index.html', '<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js', '<%= config.app %>/**/*.css']
+            }
+        },
 
-                connect: {
-                    options: {
-                        port: 9000,
-                        livereload: 35729,
-                        hostname: '127.0.0.1'
-                    },
-                    test: {
-                        options: {
-                            base: ['app'],
-                            port: 9001,
-                            hostname: process.env.HOSTNAME
-                        }
-                    },
-                    livereload: {
-                        options: {
-                            open: true,
-                            middleware: function (connect)
-                            {
-                                return [connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                hostname: '127.0.0.1'
+            },
+            test: {
+                options: {
+                    base: ['app'],
+                    port: 9001,
+                    hostname: process.env.HOSTNAME
+                }
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect)
+                    {
+                        return [connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
 
-                                ];
-                            }
-                        }
-                    }
-                },
-                jshint: {
-                    default: {
-                        options: {
-                            jshintrc: true
-                        },
-                        files: {
-                            src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']
-                        }
-                    },
-                    verify: {
-                        options: {
-                            jshintrc: true,
-                            reporter: 'checkstyle',
-                            reporterOutput: 'target/jshint.xml'
-                        },
-                        files: {src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']}
+                        ];
                     }
                 }
             }
-    );
+        },
+        jshint: {
+            default: {
+                options: {
+                    jshintrc: true
+                },
+                files: {
+                    src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']
+                }
+            },
+            verify: {
+                options: {
+                    jshintrc: true,
+                    reporter: 'checkstyle',
+                    reporterOutput: 'target/jshint.xml'
+                },
+                files: {src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']}
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: 'app'
+            },
+            src: ['**']
+        }
+    });
 
     grunt.registerTask('serve', ['connect:livereload', 'watch']);
 
